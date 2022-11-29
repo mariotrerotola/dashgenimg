0dd3b86c5e526b78a44696c8623503d9c516f9f6
mariotrerotola <mariotrerotola@gmail.com>
2022-11-29 14:18:52 +0100

    b1

--- generate_glasses.py
+++
@@ -1,6 +0,0 @@
-import openai
-openai.api_key = "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"  # supply your API key however you choose
-
-image_resp = openai.Image.create(prompt="one trendy sunglasses unisex colorful spring", n=4, size="512x512")
-
-print(image_resp)
